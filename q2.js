190890d0ace30f1cd2765306122d4326d95ea2c5
Ghaida moh <gigi@test.com>
2022-06-07 02:48:35 +0300

    hw3 added

---
+++ q2.js
@@ -0,0 +1,45 @@
+let items=
+    {Milk : 8,
+        Papers:17,
+        Egg:null,
+        Cheese:null,
+        Tissues:null,
+        Biscuits:null,
+        Banana:null,
+        water:null,
+        Bakery:null,
+        Bread:null,
+        Meat :null,
+        Seafood:null,
+        Pasta :null,
+        Rice:null,
+        Oil:null,
+        Sauces:null,
+        Salad:null,
+        Cereals:null,
+        Soups :null,
+        Canned_Goods:null,
+        Frozen_Foods:null,
+        Dairy:null}
+
+
+        //let price={};
+        for (let price of items ){
+ let collect = require('collect.js');
+
+//let words = ['forest', 'wood', 'sky', 'cloud'];
+
+ price= collect(items);
+
+if (items.every(e => e==null)){
+   
+      console.log(item.length*2);  
+       price+3;
+} 
+if(items.every(e=> e>=3)) {
+
+}
+{
+
+}
+        }
