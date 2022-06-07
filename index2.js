1e971d9a9e3627e43b9b2136d46b1289d21410f9
Meshal1172 <mr.meeshal@hotmail.com>
2022-06-07 12:25:08 +0300

    Meshal

---
+++ index2.js
@@ -0,0 +1,40 @@
+const items = {
+    Milk : 8,
+    Papers:15,
+    Egg:null,
+    Cheese:null,
+    Tissues:null,
+    Biscuits:null,
+    Banana:null,
+    water:null,
+    Bakery:null,
+    Bread:null,
+    Meat :null,
+    Seafood:null,
+    Pasta :null,
+    Rice:null,
+    Oil:null,
+    Sauces:null,
+    Salad:null,
+    Cereals:null,
+    Soups :null,
+    Canned_Goods:null,
+    Frozen_Foods:null,
+    Dairy:null}
+
+
+    function tem(){
+        
+    for(let i in items) {
+         if(items[i] == null) {
+        
+      if(i.includes('s')) {
+         items[i]=i.length*2+3;
+        }else {
+         items[i]=i.length*2
+            }  }
+        
+    console.log(i+ ' '+items[i])
+           }
+        }
+        tem()    
