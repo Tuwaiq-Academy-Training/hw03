dcd9debf2519a0884f5e08f9199382020e0b041e
Ghaida moh <gigi@test.com>
2022-06-07 10:07:09 +0300

    final update

---
+++ finalq3.js
@@ -0,0 +1,34 @@
+let Names = ['Maram','Khaild','Mohammed'];
+let ages = [25,30,20];
+let jobs = ['project manager','engineer','accountent'];
+let hobbies = ['swimming','chess','eating'];
+
+
+let obj1=
+    [{Name:'maram',
+    ages:25,
+    jobs:'project manager',
+    hobbies:'swimming'},
+
+    {
+     Name:'khalid',
+     ages:30,
+     jobs:'engineer',
+     hobbies:'chess'},
+     {  
+     Name:'mohammed',
+     ages:20,
+     jobs:'accountent',
+     hobbies:'eating'}
+  ];
+  let newObj=Object.assign(obj1);
+  console.log(newObj);
+
+
+
+
+
+
+
+
+
